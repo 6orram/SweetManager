2850c1dd6536458363e0a6ff0b78228d6f7e2763
Abderrahmane Gorram <119705007+6orram@users.noreply.github.com>
2022-12-07 02:03:19 +0100

    Add files via upload

---
+++ SweetManager_Source_Code.py
@@ -0,0 +1,27 @@
+print(""" 
+  --------------------------------------------------------------------
+    ______                                                       
+   /      \                                                      
+  |  $$$$$$\  ______    ______    ______   ______   ______ ____  
+  | $$___\$$ /      \  /      \  /      \ |      \ |      \    \ 
+  | $$    \ |  $$$$$$\|  $$$$$$\|  $$$$$$\ \$$$$$$\| $$$$$$\$$$$\ 
+  | $$$$$$$\| $$  | $$| $$   \$$| $$   \$$/      $$| $$ | $$ | $$
+  | $$__/ $$| $$__/ $$| $$      | $$     |  $$$$$$$| $$ | $$ | $$
+   \$$    $$ \$$    $$| $$      | $$      \$$    $$| $$ | $$ | $$
+    \$$$$$$   \$$$$$$  \$$       \$$       \$$$$$$$ \$$  \$$  \$$
+
+  ------------------------ Sweet Manager --------------------------                                                                                                                                                                                        
+  """)
+
+from mytools import DownloadTools
+from tkinter import *
+
+main_window = Tk()
+
+
+main_window.title('Sweet Manager')
+main_window.geometry('1200x720')
+main_window.iconbitmap(r"C:\Users\Abderrahmane\Documents\Coding\Projects\Dawnload Manager\file\Logo.ico")
+
+
+main_window.mainloop()
